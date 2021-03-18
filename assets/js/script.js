b55a872022a716db6a2cf22c3c12859fa92941d1
Owen Dodd <owen.dodd@nytimes.com>
2021-03-18 00:30:43 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@ $('.menu-icon').click(function () {
   $(".menu-item").fadeToggle().css('display', 'flex');
   $(".bar").toggleClass('x');
 });
+
+$('.link-icon').click(function () {
+  $(".link-item").fadeToggle().css('display', 'flex');
+})
